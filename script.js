1456453ede934b1949c728776c0c460489fb187a
tou-shiro <147031879+tou-shiro@users.noreply.github.com>
2024-10-07 03:46:07 +0000

    completed the basic function
    completed the basic function but still need to brush up

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ phrase = prompt("Enter a phrase");
 document.write("You entered - ", phrase);
 
 // Your pig-latin translation code here
+let phrase = prompt("Enter a phrase");
+document.write("You entered: " + phrase + "<br>");
+
+// Your Pig Latin translation code here
+
+if (phrase.charAt(0) === "a" || phrase.charAt(0) === "e" || phrase.charAt(0) === "i" || phrase.charAt(0) === "o" || phrase.charAt(0) === "u") {
+  document.write(phrase + "way");
+} 
+} 
